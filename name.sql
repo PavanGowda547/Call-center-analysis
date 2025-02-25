3b684b24038db9b5183ae484bfe5d96461df2437
Pavan Gowda R <pavangowdanyr63@gmail.com>
2025-02-25 18:26:31 +0530

    uploading sql file

--- name.sql
+++ name.sql
@@ -15,6 +15,8 @@ select * from call_records limit 10;
 
 
 
+
+
 -- 2. Data Cleaning & Handling Missing Values
 -- Identify Missing Values
 select 
@@ -46,7 +48,7 @@ select * from call_records limit 10;
 -- The Dataset contains null values, let's check the null values in a single column.
 select * from call_records where speed_of_answer_in_seconds is null;
 
--- Replacing null values in the dataset
+-- Replacing values with defaults
 UPDATE call_records
 SET 
     avgtalkduration = COALESCE(avgtalkduration, '0'),
@@ -59,7 +61,87 @@ SET
 -- The column does not contain null values
 select count(*) from call_records where answered_yes_no is null;
 
+-- Data Type conversion
+alter table call_records
+alter column avgtalkduration type time
+using 
+case 
+when avgtalkduration='0' then '00:00:00'::time
+else avgtalkduration::time
+end;
+
+select * from call_records;
+
+
+
 
 
 
 -- 3. Descriptive Statistics & Trends
+-- Find the Earliest & Latest Call Date
+select min(date) as first_date, max(date) as last_date
+from call_records;
+
+-- Average Call Duration
+select avg(avgtalkduration) as avg_talk_duration 
+from call_records;
+
+-- Calls Answered vs. Not Answered
+select answered_yes_no, count(*) as call_count
+from call_records
+group by answered_yes_no;
+
+--Calls Resolved vs. Not Resolved
+select resolved, count(*) as count
+from call_records
+group by resolved;
+
+
+
+
+
+
+-- 4. Performance Analysis
+-- Top 5 Agents with the most calls
+select agent, count(*) as total_calls
+from call_records
+group by agent
+order by total_calls desc;
+
+-- Speed of Answering Analysis
+select agent, avg(speed_of_answer_in_seconds) as avg_speed_answer
+from call_records
+group by agent
+order by avg_speed_answer asc;
+
+-- Customer Satisfaction Analysis
+-- Find the most common call topics
+select topic, count(*) as count
+from call_records
+group by topic
+order by count desc;
+
+-- Find the average satisafaction rating per topic 
+select topic, avg(satisfaction_rating) as avg_satisfaction
+from call_records
+group by topic
+order by avg_satisfaction desc;
+
+-- Time Based Analysis
+select date, count(*) as total_calls
+from call_records
+group by date
+order by date;
+
+-- Unresolved Calls
+select topic, count(*) as unresolved_cases
+from call_records
+where resolved = 'N'
+group by topic
+order by unresolved_cases desc;
+
+-- Call that has took maximum amount of time in dataset
+select call_id, agent, speed_of_answer_in_seconds 
+from call_records
+order by speed_of_answer_in_seconds desc
+limit 10;
